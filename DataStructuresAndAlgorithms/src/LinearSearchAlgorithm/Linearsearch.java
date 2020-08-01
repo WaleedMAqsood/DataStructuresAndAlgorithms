06cda64683e3821b4e71d495ab61652f0a41b465
Waleed Maqsood <Waleed Maqsood@DESKTOP-R0OLOAE>
2020-08-01 15:12:22 -0500

    Update

---
+++ DataStructuresAndAlgorithms/src/LinearSearchAlgorithm/Linearsearch.java
@@ -0,0 +1,32 @@
+package LinearSearchAlgorithm;
+
+public class Linearsearch {
+
+	public static void main(String[] args) {
+		int[] array = { 2, 44, 33, 28, 11, 4, 155, 221, 234 };
+		int valueToFind = 28;
+
+		int result = searchValue(array, valueToFind);
+
+		if (result == -1) {
+			System.out.println("The value does not exist ");
+
+		} else {
+
+			System.out.println("The value is at index " + result);
+
+		}
+	}
+
+	public static int searchValue(int[] array, int value) {
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+
+		}
+		return -1;
+
+	}
+}
