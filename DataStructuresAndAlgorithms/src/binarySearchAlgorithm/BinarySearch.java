59126493e902c4c2a51b4d24b2e19393ceebd454
Waleed Maqsood <Waleed Maqsood@DESKTOP-R0OLOAE>
2020-08-15 11:40:09 -0500

    Update

--- DataStructuresAndAlgorithms/src/binarySearchAlgorithm/BinarySearch.java
+++ DataStructuresAndAlgorithms/src/binarySearchAlgorithm/BinarySearch.java
@@ -6,7 +6,7 @@ package binarySearchAlgorithm;
 public class BinarySearch {
 
 	public static int binarysearch(int[] array, int key, int first, int last) {
-
+//update
 		while (first <= last) {
 
 			/*
