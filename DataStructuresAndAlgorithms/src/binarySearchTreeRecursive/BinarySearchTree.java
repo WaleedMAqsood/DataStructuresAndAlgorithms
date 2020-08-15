2aa5fa4d0332bce8da6506b991367065b7f90d30
Waleed Maqsood <Waleed Maqsood@DESKTOP-R0OLOAE>
2020-08-15 10:33:42 -0500

    Updated BinarySearchTree

--- DataStructuresAndAlgorithms/src/binarySearchTreeRecursive/BinarySearchTree.java
+++ DataStructuresAndAlgorithms/src/binarySearchTreeRecursive/BinarySearchTree.java
@@ -1,6 +1,7 @@
 /*
- * Insert Operation in Binary search tree using Recursion 
+ * Insert and delete Operation in Binary search tree using Recursion 
  */
+
 package binarySearchTreeRecursive;
 
 public class BinarySearchTree {
@@ -20,21 +21,109 @@ public class BinarySearchTree {
 	 */
 	Node insertKey(Node root, int key) {
 
+		// If the tree is empty return a new node
 		if (root == null) {
 
 			return new Node(key);
 		}
-
+		// if the key is less than the root then recur left subtree
 		else if (key < root.key) {
 
 			root.left = insertKey(root.left, key);
 		}
+		// if the key is greater than root then recur right subtree
 
 		else if (key > root.key) {
 
 			root.right = insertKey(root.right, key);
 		}
 		return root;
+
+	}
+
+	/*
+	 * This method calls deleteKey method
+	 * 
+	 */
+	public void delete(int key) {
+
+		root = deleteKey(root, key);
+	}
+
+	/*
+	 * Method to perform delete operator in BST
+	 */
+
+	public Node deleteKey(Node root, int key) {
+
+		// if the tree is empty
+		if (root == null)
+			return null;
+
+		// if the key is less than the root then recur left subtree
+		if (key < root.key) {
+			root.left = deleteKey(root.left, key);
+		}
+
+		// if the key is greater than root then recur right subtree
+		else if (key > root.key) {
+			root.right = deleteKey(root.right, key);
+		}
+
+		// If the key is same as the root get ready to delete the key
+		else {
+
+			// Case 1: Node has no children
+			if (root.left == null && root.right == null) {
+				return null;
+			}
+
+			// Case 2: If Node has only one child
+			else if (root.left == null) {
+
+				return root.right;
+
+			} else if (root.right == null) {
+
+				return root.left;
+
+			}
+
+			// Case 3: if Node has two children.Can just be an else
+			else if (root.left != null && root.right != null) {
+
+				// find inOrder successor node and copy the content of the successor node to
+				// the deletion node
+
+				root.key = minNodeRight(root.right);
+
+				// delete the inOrder successor
+				root.right = deleteKey(root.right, root.key);
+
+			}
+
+		}
+
+		return root;
+
+	}
+
+	/*
+	 * Method to find the inOrder successor
+	 */
+	int minNodeRight(Node root) {
+
+		int minNodeRight = root.key;
+
+		while (root.left != null) {
+
+			minNodeRight = root.left.key;
+
+			root = root.left;
+
+		}
+
+		return minNodeRight;
 
 	}
 
--- DataStructuresAndAlgorithms/src/binarySearchTreeRecursive/Main.java
+++ DataStructuresAndAlgorithms/src/binarySearchTreeRecursive/Main.java
@@ -6,13 +6,32 @@ public class Main {
 
 		BinarySearchTree tree = new BinarySearchTree();
 
-		tree.insert(5);
-		tree.insert(15);
-		tree.insert(20);
+		tree.insert(50);
 		tree.insert(25);
+		tree.insert(60);
+		tree.insert(20);
+		tree.insert(52);
+		tree.insert(30);
+		tree.insert(55);
+		tree.insert(80);
 		tree.insert(30);
 
-		System.out.println("Print the nodes in Inorder traversal");
+		System.out.println("Printing the nodes in Inorder traversal");
+		tree.inOrder();
+
+		System.out.println("\nDeleting 30");
+		tree.delete(30);
+		System.out.println("Printing the nodes after deleting");
+		tree.inOrder();
+
+		System.out.println("\nDeleting 60");
+		tree.delete(60);
+		System.out.println("Printing the nodes after deleting");
+		tree.inOrder();
+
+		System.out.println("\nDeleting 50");
+		tree.delete(50);
+		System.out.println("Printing the nodes after deleting");
 		tree.inOrder();
 
 	}
