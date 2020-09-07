8b57a25794a0bde5ce7fb2fff9c4f495c563a632
Waleed Maqsood <Waleed Maqsood@DESKTOP-R0OLOAE>
2020-09-06 22:43:43 -0500

    Queue Implementation Using Array

---
+++ DataStructuresAndAlgorithms/src/QueueImplementationUsingArray/Main.java
@@ -0,0 +1,27 @@
+package QueueImplementationUsingArray;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Queue q = new Queue();
+
+		// Inserting elements in Queue
+		q.enQueue(32);
+		q.enQueue(33);
+		q.enQueue(65);
+		q.enQueue(32);
+		// Printing Queue
+		q.show();
+
+		// Printing the front element in Queue
+		System.out.println("The front element in queue is:" + q.peek());
+
+		// Deleting an Element in Queue
+		q.deQueue();
+		// Printing QUeue
+		q.show();
+
+	}
+
+}
---
+++ DataStructuresAndAlgorithms/src/QueueImplementationUsingArray/Queue.java
@@ -0,0 +1,103 @@
+package QueueImplementationUsingArray;
+
+public class Queue {
+
+	int capacity = 5;
+	int arr[] = new int[capacity];
+	int rear;
+	int front;
+	int size;
+
+	/*
+	 * Method to add an element to Queue
+	 */
+	public void enQueue(int data) {
+
+		if (isFull()) {
+			System.out.print("Queue is full");
+			System.exit(1);
+		}
+		System.out.println(data + " Inserted in Queue");
+		arr[rear] = data;
+		rear = (rear + 1) % 5;
+		size++;
+	}
+
+	/*
+	 * Method to remove an element from Queue
+	 */
+	public int deQueue() {
+
+		if (isEmpty()) {
+			System.out.print("Queue is Empty");
+			System.exit(1);
+
+		}
+
+		int data = arr[front];
+		System.out.println("Deleting front element in Queue:" + data);
+		front = (front + 1) % 5;
+		size--;
+		return data;
+
+	}
+
+	/*
+	 * Method to return the size of Queue
+	 */
+	public int size() {
+		return size;
+	}
+
+	/*
+	 * Method to check if the Queue is full or not
+	 */
+	public boolean isFull() {
+
+		return size() == capacity;
+
+	}
+
+	/*
+	 * Method to check if Queue is empty or not
+	 */
+	public Boolean isEmpty() {
+
+		return size() == 0;
+
+	}
+
+	/*
+	 * Method to return the front element in Queue
+	 */
+	public int peek() {
+
+		if (isEmpty()) {
+			System.out.print("Queue is Empty");
+			System.exit(1);
+
+		}
+		return arr[front];
+
+	}
+
+	/*
+	 * Method to display Queue
+	 */
+	public void show() {
+
+		if (isEmpty()) {
+			System.out.print("Queue is Empty");
+			System.exit(1);
+		}
+
+		System.out.println("Printing Element in Queue:");
+		for (int i = 0; i < size; i++) {
+
+			System.out.print(arr[front + i] + " ");
+
+		}
+		System.out.println();
+
+	}
+}
